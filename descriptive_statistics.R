bcaafc2764e22b3ffade5fcdf6a99548a17d6cdf
Dmitri Timasjov <timasjov.dmitri@gmail.com>
2015-05-18 23:10:49 +0300

    added initial t-tests

--- descriptive_statistics.R
+++ descriptive_statistics.R
@@ -1,4 +1,13 @@
 #Read in data
 data = read.table("data/processedData.txt", header=T)
 
-#Do something useful
+#Find correlation between attributes using t-test
+t.test(data$s.nr, data$place)
+
+t.test(data$particip.time, data$place)
+
+data$age.group.number = as.numeric(substr(data$age.group, 2, 3))
+t.test(data$age.group.number, data$place)
+
+data$country.number = as.numeric(data$country)
+t.test(data$country.number, data$place)
