0d45c2e8994337b155062b64c0425db95cfc65dc
Dmitri Timasjov <timasjov.dmitri@gmail.com>
2015-05-21 22:58:50 +0300

    implemented basic regression model fit, added standalone configuration file

---
+++ regression.R
@@ -0,0 +1,26 @@
+#Read in data
+data = read.table("data/processedData.txt", header=T)
+
+#Split final times into 10 groups
+data$timeCategory <- ntile(data$time, 10)
+
+
+#Fit model using ANOVA algorithm
+fit <- rpart(timeCategory ~ particip.time + s.nr, data=data)
+printcp(fit)
+summary(fit)
+rpart.plot(fit, type=3, extra=1)
+
+
+#Fit model using using linear model
+lmfit <- lm(time ~ country + age.group, data=data)
+form <- as.matrix(coef(lmfit))
+rownames(form) <- gsub("try", "try == ", rownames(form) )
+rownames(form) <- gsub("oup", "oup == ", rownames(form) )
+rownames(form)[1] <- "Base"
+cat(paste( form, paste("(", rownames(form), ")" ), sep="*", collapse="+\n") )
+
+#Fit model using M5P algorithm
+fit <- M5P(timeCategory ~ particip.time + s.nr, data=data)
+plot(fit)
+summary(fit)
